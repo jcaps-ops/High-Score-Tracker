b1e09035132afa10927b7da6363da81321d96dc7
INTELION11 <jacob.quezada@ucas-edu.net>
2026-02-12 17:26:43 +0000

    made nathans code

--- Jacob/UserRegstration.py
+++ Jacob/UserRegstration.py
@@ -29,16 +29,16 @@
 #                 open pass_a_user.csv mode="a newline is space, as file:  
 #                    writer is equal to csv.writer(file)  
 #                    writer.writerow([option, password])  
-#                print("User added")  
+#                display User added  
 #             
 #            except:  
-#                print("Could not write to file.")  
+#                display Could not write to file  
 #
-#regis()
+#run function regis()
 
   
 import csv  
-  
+
 def regis():  
     loop = True  
     while loop:  
@@ -47,7 +47,7 @@ def regis():
             loop = False  
               
         try:  
-            with open("pass_a_user.csv", mode="r") as file:  
+            with open("Jacob/pass_a_user.csv", mode="r") as file:  
                 reader = csv.reader(file, delimiter=',')   
                 users = []  
                 for line in reader:  
@@ -75,32 +75,4 @@ def regis():
 regis()
 
   
-def login():  
-    loop = True  
-    while loop:  
-        option = input("What is your username?\n").strip()  
-        if option == "exit":  
-            loop = False  
-            continue  
-        try:  
-            with open("pass_a_user.csv", mode="r") as file:  
-                reader = csv.reader(file, delimiter=',')  
-                header = next(reader)  
-                users = {}  
-                for line in reader:  
-                    users[line[0]] = line[1]  
-        except:  
-            print("cant find csv")  
-            continue  
-  
-        if option in users:  
-            password = input("Enter your password:\n").strip()  
-            if password == users[option]:  
-                print("Login successful!")  
-                # Optionally break here if you want to stop after a successful login  
-            else:  
-                print("Incorrect password.")  
-        else:  
-            print("Username not found.")  
-  
-login()  
+d
