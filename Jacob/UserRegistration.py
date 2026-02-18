c9986fc98fc7e80a8d5213141b56a4a4174002b8
INTELION11 <jacob.quezada@ucas-edu.net>
2026-02-18 21:08:33 +0000

    added sthreinghtn cheker

--- Jacob/UserRegistration.py
+++ Jacob/UserRegistration.py
@@ -1,40 +1,4 @@
 # JQ 2nd Loginout
-#import csv  
-#  
-#define regis() as a function:  
-#    loop is equal to True  
-#    while loop is true:  
-#        option is equal to display "What is your username? 
-#        if option is equal to exit:  
-#            loop is equal to False  
-#            return  
-#        try:  
-#            open pass_a_user.csv mode r as file  
-#                reader is equal to csv.reader(file, delimiter=',')   
-#                users is a list  
-#                for line in reader:  
-#                    append line[0]: line[1] to users
-#        except:  
-#           display cant find csv  
-#           continue  
-#        found is equlat to False  
-#        for user in users:  
-#            if option is in user:  
-#                display already in data base  
-#                found is equal to True  
-#                break  
-#        if found is false:  
-#            password is equal to display  good, now select your password  
-#            try:  
-#                 open pass_a_user.csv mode="a newline is space, as file:  
-#                    writer is equal to csv.writer(file)  
-#                    writer.writerow([option, password])  
-#                display User added  
-#             
-#            except:  
-#                display Could not write to file  
-#
-#run function regis()
 import random as r  
 import time as t  
 import sys  
@@ -53,13 +17,81 @@ def processing(text="Processing", duration=3, speed=0.5):
     sys.stdout.write('\r' + ' ' * (len(text) + 3) + '\r')
     sys.stdout.flush()
     print(text)
-
 def sprint(text, delay=0.025):  
     for char in text:  
         sys.stdout.write(char)  
         sys.stdout.flush()  
         t.sleep(delay)  
-
+def pass_cheker():
+    password = input("what is your password \n").strip()  # ask for password and clean it  
+    points = 0  # start points at zero  
+  
+    #length = len(password)  
+    #list special_character  
+    #list numbers  
+    #characters is list(password)  
+    #loop for letter in characters  
+  
+    special_characters = "!@#\$%^&*()_+-=[]{|;:,}.<>?)"  # all the weird symbols  
+    numbers = "1234567890"  # just the digits for checking  
+    # set up flags to see if each thing is found  
+    has_num = False  # did we see a number yet  
+    has_special = False  # did we see a special character yet  
+    has_upper = False  # any uppercase found  
+    has_lower = False  # any lowercase found  
+  
+    # go through each character in password  
+    for char in password:  
+        # if it's a number, remember that  
+        if char in numbers:  
+            has_num = True  
+        # if it's a special, remember that  
+        if char in special_characters:  
+            has_special = True  
+        # if uppercase, mark it  
+        if char.isupper():  
+            has_upper = True  
+        # if lowercase, mark it  
+        if char.islower():  
+            has_lower = True  
+  
+    #if length >= 8 then points += 1  
+    if len(password) >= 8:  # long enough  
+        points += 1  
+    #if has_num == True then points += 1  
+    if has_num:  # saw a number  
+        points += 1  
+    #if has_special == True then points += 1  
+    if has_special:  # saw a special char  
+        points += 1  
+    #if has_upper == True then points += 1  
+    if has_upper:  # saw uppercase  
+        points += 1  
+    #if has_lower == True then points += 1  
+    if has_lower:  # saw lowercase  
+        points += 1  
+  
+    #Meeting the length requirement == True: +1 point  
+    #Containing uppercase letters == True: +1 point  
+    #Containing lowercase letters == True: +1 point  
+    #Containing numbers == True: +1 point  
+    #Containing special characters == True: +1 point  
+    
+    #if points == 1  
+    if points == 1 or points == 2:  
+        print("password is weak \n")  # say weak for 1 or 2  
+        print(f"{points} point(s)")  
+    #display password is medium  
+    elif points == 3:  
+        print("password is medium\n")  # 3 gets medium  
+    #display password is good  
+    elif points == 4:  
+        print("password is good \n")  # 4 gets good  
+    #display password is strong  
+    elif points == 5:  
+        print("password is strong \n")  # all 5, very strong  
+    else:   
+        print("please try again")  # no points, something went wrong  
 
 def regis():  
     loop = True  
@@ -96,9 +128,6 @@ def regis():
              
             except:  
                 sprint("\033[38;2;49;125;125mCould not write to file.")  
-
-
-
 def login():  
     loop = True  
     while loop:  
@@ -126,6 +155,7 @@ def login():
                 sprint("\033[38;2;255;1;1mIncorrect password, i thought you had an IQ higher than 85.\n")  
         else:  
             sprint("\033[38;2;255;1;1mYou Spell Like my grandma.\n")  
+        
 while True:
     ei = input("\033[38;2;0;125;1mlogin or register\n").strip().lower()
     if ei == "login":
@@ -136,5 +166,3 @@ while True:
     else:
         print("\033[38;2;255;1;1mtry again")
         continue
-
-# mol,man
