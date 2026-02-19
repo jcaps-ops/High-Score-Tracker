a098a1f44cd5f9fac153213c3f28beb8f320c994
INTELION11 <jacob.quezada@ucas-edu.net>
2026-02-19 17:50:09 +0000

    added helper file and updated code

--- High_score_tracker/Jacob/UserRegistration.py
+++ High_score_tracker/Jacob/UserRegistration.py
@@ -1,28 +1,8 @@
-# JQ 2nd Loginout
-import random as r  
-import time as t  
-import sys  
+# JQ 2nd Loginout 
 import csv  
-import os
-
-
-def clearr():  
-    os.system('cls' if os.name == 'nt' else 'clear')  
-def processing(text="Processing", duration=3, speed=0.5):
-    end_time = t.time() + duration
-    while t.time() < end_time:
-        for dots in range(4):
-            sys.stdout.write('\r' + text + '.' * dots + ' ' * (3 - dots))
-            sys.stdout.flush() 
-            t.sleep(speed)
-    sys.stdout.write('\r' + ' ' * (len(text) + 3) + '\r')
-    sys.stdout.flush()
-    print(text)
-def sprint(text, delay=0.025):  
-    for char in text:  
-        sys.stdout.write(char)  
-        sys.stdout.flush()  
-        t.sleep(delay)  
+from helper import sprint
+from helper import clearr
+from helper import processing
 # functions used everywhere^^^^^
 
 def regis():
