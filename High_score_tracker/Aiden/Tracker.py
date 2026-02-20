01d503ef64eb3e46d42436584ed4db5e5df6ddf1
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-02-20 17:49:48 +0000

    finished the tracking part of my section

--- High_score_tracker/Aiden/Tracker.py
+++ High_score_tracker/Aiden/Tracker.py
@@ -1,2 +1,35 @@
 #AS 2nd High score tracker
+import csv
 
+def score_tracking(username, score):
+    all = []
+    with open("High_score_tracker/Aiden/highscores.csv", mode= 'r') as sample:
+        reader = csv.reader(sample)
+        for line in reader:
+            if line[0] == 'Username':
+                pass
+            else:
+                all.append({'Username':line[0] ,'Highscore':line[1] ,'Latest Score':line[2]})
+        for line in all:
+            if line["Username"] == username:
+                newline = line
+                all.remove(line)
+                newline['Latest Score'] = score
+                if score > int(newline['Highscore']):
+                    newline['Highscore'] = score
+                else:
+                    pass
+                print(newline)
+            else:
+                pass
+        try:
+            all.append(newline)
+        except:
+            all.append({'Username':username ,'Highscore':score ,'Latest Score':score})
+    with open("High_score_tracker/Aiden/highscores.csv", mode= 'w', newline= '') as sample:
+        fieldnames = ['Username','Highscore','Latest Score']
+        writer = csv.DictWriter(sample, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(all)
+
+score_tracking('Steve', 100)
