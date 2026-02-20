11b68231a969673961e8433ac78d0456a35d36e2
INTELION11 <jacob.quezada@ucas-edu.net>
2026-02-20 17:50:20 +0000

    fixed problems

--- High_score_tracker/Main.py
+++ High_score_tracker/Main.py
@@ -1,1 +1,15 @@
 #This is the main file
+
+from Jacob.UserRegistration import login, regis
+from helper import sprint, clearr, processing
+while True:
+    ei = input("\033[38;2;0;125;1mlogin or register\n").strip().lower()
+    if ei == "login":
+        print(login()) 
+    elif ei == "register": 
+        print(regis())
+    else:
+        clearr()
+        print("\033[38;2;255;1;1mtry again")
+    
+        continue
