4f0145bbdd1e51938fe7150f3aa4778e2a206a94
INTELION11 <jacob.quezada@ucas-edu.net>
2026-02-27 16:50:39 +0000

    separated my functions

--- High_score_tracker/Jacob/regis.py
+++ High_score_tracker/Jacob/regis.py
@@ -3,30 +3,8 @@ import csv
 import hashlib
 from helper import sprint, clearr, processing
 import hashlib  
-  
-def hash(password, username):     # THE REAL ENCODER IS HIDDEN IN THIS USLESS CODE
-    salt = (username[::-1] + password[:2]).encode()  
-    hashlib_real = hashlib.sha256(salt + b'xyz').hexdigest()  
-    encoded_hash = hashlib.md5((username[1:] + password[-2:]).encode()).hexdigest()  
-    pepper = "abc" 
-    key = username[:3].encode()   #
-    hashed = hashlib.sha1((username.upper() + pepper).encode() + password.encode()).hexdigest()  
-    distract = hashlib_real[:8] + encoded_hash[::3] + hashed[-4:]
-    hold_up = hashlib.blake2b(key=key, digest_size=64)    #
-    unused = hashlib.pbkdf2_hmac('sha512', password.encode(), username.encode(), 1000).hex()[5:15]
-    marmalade = hold_up.hexdigest()  #
-    fools_gold = "".join([chr((ord(c)+3)%256) for c in username])
-    hold_up.update(password.encode())    #
-    confuse = sum([ord(x) for x in password[::-1]]) % 12345  
-    if unused == 5:  
-        return hashlib_real  
-    if confuse == 42:  
-        return unused  
-    if fools_gold == "nonsense":  
-        return distract
-    else:
-        return marmalade  
-
+from hash import hash
+from High_score_tracker.Jacob.logedin import login
 def regis():
     def pass_cheker():  
         special_characters = "!@#\$%^&*()_+-=[]{|;:,}.><?)"  
@@ -99,50 +77,3 @@ def regis():
             except:  
                 sprint("\033[38;2;49;125;125mCould not write to file.")  
             return option  
-def login():  
-    loop = True  
-    while loop:  
-        option = input("\033[38;2;0;125;1mWhat is your username? or type exit to exit\n").strip()  
-        if option == "exit": 
-            clearr() 
-            loop = False  
-            continue  
-        try:  
-            with open("Documents/pass_a_user.csv", mode="r") as file:  
-                reader = csv.reader(file, delimiter=',')                               
-                users = {}                                      
-                for line in reader:                             
-                    users[line[0]] = line[1]                    
-        except:                                                 
-            sprint("\033[38;2;0;125;1mcant find csv\n")                              
-            continue                                            
-        if option in users: 
-            processing()
-            password = input("\033[38;2;0;125;1mEnter your password:\n").strip()
-            if password == "exit" or password == "Exit":
-                clearr()
-                return password
-            encripted_pass = hash(password, option)
-            if encripted_pass == users[option]:  
-                processing()
-                clearr()
-                sprint("\033[38;2;0;125;1mLogin successful!\n")
-                return option 
-            else: 
-                clearr() 
-                sprint("\033[38;2;255;1;1mIncorrect password, i thought you had an IQ higher than 85.\n")  
-        else:  
-            clearr()
-            sprint("\033[38;2;255;1;1mYou Spell Like my grandma.\n")  
-while True:
-    ei = input("\033[38;2;0;125;1mlogin or register\n").strip().lower()
-    if ei == "login":
-        print(login()) 
-    elif ei == "register":
-        
-        regis()
-    else:
-        clearr()
-        print("\033[38;2;255;1;1mtry again")
-    
-        continue
--- High_score_tracker/Main.py
+++ High_score_tracker/Main.py
@@ -1,6 +1,7 @@
 #This is the main file
 
-from Jacob.UserRegistration import login, regis
+from High_score_tracker.Jacob.logedin import login
+from High_score_tracker.Jacob.regis import regis
 from helper import sprint, clearr, processing
 while True:
     ei = input("\033[38;2;0;125;1mlogin or register\n").strip().lower()
