ecad15d905f05d5d3b7023f16a99aa4e16bd7c90
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2026-03-04 16:59:21 +0000

    working on main

--- High_score_tracker/Main.py
+++ High_score_tracker/Main.py
@@ -3,14 +3,21 @@
 from Jacob.logedin import login
 from Jacob.regis import regis
 from helper import sprint, clearr, processing
+from Aiden.Tracker import score_tracking, highscores
+from Jaxon.CasinoRoyale import 
+
 while True:
     ei = input("\033[38;2;0;125;1mlogin or register\n").strip().lower()
     if ei == "login":
-        print(login()) 
+        username = login()
+        print(username) 
     elif ei == "register": 
-        print(regis())
+        username = regis()
+        print(username) 
     else:
         clearr()
         print("\033[38;2;255;1;1mtry again")
     
         continue
+    
+    score_tracking()
