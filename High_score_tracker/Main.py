ad7805b29e86dd16ba56b41509bcb866cd337a26
Hunter <nathan.hasson@ucas-edu.net>
2026-03-05 19:39:48 +0000

    I added pseudocode to main and also fixed a typo when calling a function

--- High_score_tracker/Main.py
+++ High_score_tracker/Main.py
@@ -1,13 +1,17 @@
-#This is the main file
-
+#This is the main file for high score tracker. AC NH JQ JC 2nd
+#Import everything
 from Jacob.logedin import login
 from Jacob.regis import regis
 from helper import sprint, clearr, processing
 from Aiden.Tracker import score_tracking, highscores
 from Jaxon.CasinoRoyale import game
 
+#While loop
 while True:
+    #Main menu
     ei = input("\033[38;2;0;125;1m1: login\n2: register\n3: play (this will not save your score, for you did not log in)\n4: view leaderboard\n5: leave\n").strip().lower()
+
+    #login
     if ei == "1":
         username = login()
         if username == 'exit' or username == 'Exit':
@@ -16,6 +20,7 @@ while True:
             pass
         score_tracking(username, game())
 
+    #registration
     elif ei == "2": 
         username = regis()
         if username == 'exit' or username == 'Exit':
@@ -24,37 +29,47 @@ while True:
             pass
         score_tracking(username, game())
     
+    #play game
     elif ei == "3": 
         game()
     
+    #View leaderboard
     elif ei == "4": 
         clearr()
         highscores()
         print('\n\n')
         continue
 
+    #leave
     elif ei == "5": 
         break
 
+    #if invalid
     else:
         clearr()
         print("\033[38;2;255;1;1mtry again")
-    
+    #Make sure user knows to have big terminal
         continue
     print("Please use full screen")
     
-
+    #Display the high scores
     highscores()
 
+    #While loop
     while True:
+        #Ask them what they want for after the game
         uinput = input("Would you like to 1: Play Again, or 2: Leave?\n").strip()
+        #If they wanna play again
         if uinput == '1':
             try:
+                #Display the score
                 score_tracking(username, game())
                 highscores()
+            #Start the game
             except:
                 game()
-                highscores
+                highscores()
+        #leave the game
         elif uinput == '2':
             break
         else:
--- High_score_tracker/helper.py
+++ High_score_tracker/helper.py
@@ -1,8 +1,14 @@
+# AC JC JQ NH 2nd Helper functions for main
+#Import the libraries os, time as t, and sys
 import os
 import sys
 import time as t
-def clearr():  
+
+#def clear function
+def clearr():
     os.system('cls' if os.name == 'nt' else 'clear')  
+
+#def the processing function
 def processing(text="Processing", duration=3, speed=0.5):
     end_time = t.time() + duration
     while t.time() < end_time:
@@ -13,6 +19,8 @@ def processing(text="Processing", duration=3, speed=0.5):
     sys.stdout.write('\r' + ' ' * (len(text) + 3) + '\r')
     sys.stdout.flush()
     print(text)
+
+#def the speed print function
 def sprint(text, delay=0.025):  
     for char in text:  
         sys.stdout.write(char)  
